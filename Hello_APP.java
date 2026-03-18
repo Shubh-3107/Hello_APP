588816432e7de2a06748bacc8f109e7798ffe36e
Shubh-3107 <mishra.shubh031@gmail.com>
2026-03-18 03:26:52 +0000

    Coded UC2: Display Hello with Command-Line Argument

--- Hello_APP.java
+++ Hello_APP.java
@@ -2,6 +2,8 @@ class Hello_APP{
     
     public static void main(String[] args){
 
-        System.out.println("Hello, World!");
+
+        String name = args[0];
+        System.out.println("Hello," + name + "!");
     }
 }
